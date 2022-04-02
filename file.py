6be587f4352f634ed6ac047c07bf3e67c92acfa9
Matthias <48438784+speedswimmer@users.noreply.github.com>
2022-04-02 22:50:28 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,5 +1,6 @@
 #Script to handle logfiles on server. Script is scheduled to run on a regular bases e.g. each month or so
-import os, datetime, shutil
+import os, shutil
+from datetime import datetime
 import logging
 
 logging.basicConfig(filename='logging.log', filemode = 'a', level=logging.DEBUG, style="{" ,format = "{asctime} [{levelname:8}] {message}", datefmt="%d.%m.%Y, %H:%M:%S")
@@ -7,8 +8,10 @@ logging.basicConfig(filename='logging.log', filemode = 'a', level=logging.DEBUG,
 source_path = '/home/pi/Scripts/data/'
 destination = '/home/pi/Scripts/data_old/'
 
-selector = datetime.datetime.now()
-selector_final = (str(selector.month) + '-' + str(selector.year-2000) + '-log.txt')
+selector = datetime.now()
+#selector_final = (str(selector.month) + '-' + str(selector.year-2000) + '-log.txt')
+year = str(selector.year - 2000)
+
 
 if selector.day <10:
         day_value = '0'+str(selector.day)
@@ -34,7 +37,7 @@ for files in list:
                         container.append(value)
 # loop to check if target directories for each month do exist or not!
 for i in range(len(container)):
-        folder_name = (container[i]+'_21')
+        folder_name = (container[i] + '_' + year)
         if os.path.exists(destination+folder_name) == True:
                 continue
         else:
@@ -50,7 +53,7 @@ for element in container:
                                 logging.info("File {} won't be copied!".format(i))
 #                                print("File {} won't be copied!".format(i))
                         else:
-                                place = destination + element + '_21'
+                                place = destination + element + '_' + year
                                 try:
                                         # move of file from source to destination path with overriding if file exists already in destination
                                         shutil.move(os.path.join(source_path, i), os.path.join(place, i))
